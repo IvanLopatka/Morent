3b77eb97730c9c72f5baa611e2050bd8bf21dd90
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-09-13 15:47:54 +0300

    chore: remove package-lock.json and update package.json with new dependencies and CSS styles

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
+import React from 'react';
+
+export const NavigationBar: FC = () => {
+  return (
+    <nav className='flex justify-between items-center'>
+
+    </nav>
+  )
+}
