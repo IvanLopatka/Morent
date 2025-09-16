45c388901cd9889126516447beeca7982ec3589a
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-09-16 15:05:57 +0300

    feat: add viewport meta tag for improved mobile responsiveness and include MobileSearch component in Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { NavigationBar } from "@/components";
+import { MobileSearch } from "@/components";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[auto_1fr_auto] items-center justify-center font-[family-name:var(--font-geist-sans)">
+    <div className="grid min-h-screen grid-rows-[auto_1fr_auto] items-start justify-center font-[family-name:var(--font-geist-sans)">
       <NavigationBar />
+      <MobileSearch />
     </div>
   );
 }
