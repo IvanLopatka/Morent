45e4b1e5c9d5c3ec07136bf01a1ac51d7b883215
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-11-19 11:03:45 +0200

    refactor: improve Sidebar component layout and styling for car type and capacity filters

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,68 +12,107 @@ export const Sidebar = () => {
       <div className="pl-4 flex flex-col mt-12 gap-12 w-[360px] h-full ">
         <div className="flex flex-col gap-8">
           <p className="text-xs text-gray-500 font-semibold">Type</p>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="sport" />
             <Label htmlFor="sport">
-              Sport ({Cars.filter((car) => car.type === "Sportscar").length})
+              <span className="text-xl text-gray-500 font-semibold">Sport</span>
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.type === "Sportscar").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="suv" />
             <Label htmlFor="suv">
-              SUV ({Cars.filter((car) => car.type === "SUV").length})
+              <span className="text-xl text-gray-500 font-semibold">SUV</span>
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.type === "SUV").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="mpv" />
             <Label htmlFor="mpv">
-              MPV ({Cars.filter((car) => car.type === "MPV").length})
+              <span className="text-xl text-gray-500 font-semibold">MPV</span>
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.type === "MPV").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="sedan" />
             <Label htmlFor="sedan">
-              Sedan ({Cars.filter((car) => car.type === "Sedan").length})
+              <span className="text-xl text-gray-500 font-semibold">Sedan</span>
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.type === "Sedan").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="coupe" />
             <Label htmlFor="coupe">
-              Coupe ({Cars.filter((car) => car.type === "Coupe").length})
+              <span className="text-xl text-gray-500 font-semibold">Coupe</span>
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.type === "Coupe").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="hatchback" />
             <Label htmlFor="hatchback">
-              Hatchback ({Cars.filter((car) => car.type === "Hatchback").length}
-              )
+              <span className="text-xl text-gray-500 font-semibold">
+                Hatchback
+              </span>{" "}
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.type === "Hatchback").length})
+              </span>
             </Label>
           </div>
         </div>
         <div className="flex flex-col gap-8">
-          <p className="text-xs text-gray-500 font-semibold">Capacity</p>
-          <div className="flex gap-2">
+          <p className="text-xs text-gray-400 font-semibold">Capacity</p>
+          <div className="flex items-center gap-2">
             <Checkbox id="2-person" />
             <Label htmlFor="2-person">
-              2 Person ({Cars.filter((car) => car.seats === "2").length})
+              <span className="text-xl text-gray-500 font-semibold">
+                2 Person
+              </span>{" "}
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.seats === "2").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="4-person" />
             <Label htmlFor="4-person">
-              4 Person ({Cars.filter((car) => car.seats === "4").length})
+              <span className="text-xl text-gray-500 font-semibold">
+                4 Person
+              </span>{" "}
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.seats === "4").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="6-person" />
             <Label htmlFor="6-person">
-              6 Person ({Cars.filter((car) => car.seats === "6").length})
+              <span className="text-xl text-gray-500 font-semibold">
+                6 Person
+              </span>{" "}
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.seats === "6").length})
+              </span>
             </Label>
           </div>
-          <div className="flex gap-2">
+          <div className="flex items-center gap-2">
             <Checkbox id="8-or-more" />
             <Label htmlFor="8-or-more">
-              8 or More ({Cars.filter((car) => car.seats === "8").length})
+              <span className="text-xl text-gray-500 font-semibold">
+                8 or More
+              </span>
+              <span className="text-xl text-gray-400 font-semibold">
+                ({Cars.filter((car) => car.seats === "8").length})
+              </span>
             </Label>
           </div>
         </div>
