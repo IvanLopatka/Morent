5cf9d99fcc276a78049f5e2e19862c9cd09e14db
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-12-02 14:37:55 +0200

    feat: add background image to CarInfo component and enhance layout with improved styling for car details

--- src/components/CarInfo.tsx
+++ src/components/CarInfo.tsx
@@ -37,12 +37,20 @@ export const CarInfo: FC<CarInfoProps> = ({
     setRating(index + 1);
   };
   return (
-    <div className="p-8 flex w-full justify-between items-center">
-      <div className="w-[40%] bg-white rounded-lg"></div>
-      <div className="justify-center p-6 items-center flex flex-col gap-4 w-[40%] bg-white rounded-lg">
+    <div className="flex lg:flex-row mb-8 flex-col w-full gap-y-8 lg:justify-between items-stretch">
+      <div className="lg:w-[47%] pl-4 flex lg:h-max-content lg:justify-center justify-start items-center bg-cover bg-[url('/background.png')] w-full rounded-lg">
+        <Image
+          src={car?.image || ""}
+          alt={car?.name || ""}
+          width={200}
+          height={200}
+          className="w-full object-cover"
+        />
+      </div>
+      <div className="justify-center p-6 items-center flex flex-col gap-4 lg:w-[47%] w-full bg-white rounded-lg self-stretch">
         <div className="flex flex-row w-full justify-between items-center">
-          <div className="flex flex-col gap-y-1">
-            <h1 className="text-2xl font-bold">{car?.name}</h1>
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-3xl font-bold">{car?.name}</h1>
             <div className="flex gap-1 flex-row">
               {Stars.map((star, idx) => (
                 <div key={star} className="flex flex-row">
@@ -84,11 +92,34 @@ export const CarInfo: FC<CarInfoProps> = ({
           </Button>
         </div>
 
-        <p className="text-sm text-gray-500">{car?.type}</p>
-        <p className="text-sm text-gray-500">{car?.description}</p>
-        <p className="text-sm text-gray-500">${car?.price}/day</p>
-        <p className="text-sm text-gray-500">{car?.seats} seats</p>
-        <p className="text-sm text-gray-500">{car?.spending}L/day</p>
+        <p className="text-xl my-8 text-gray-500">{car?.description}</p>
+        <div className="flex w-full mb-8 flex-col gap-4">
+          <div className="flex flex-row justify-between">
+            <p className="text-xl font-semibold text-black"> {car?.type}</p>
+            <p className="text-xl font-semibold text-black">
+              {car?.seats} seats
+            </p>
+          </div>
+
+          <div className="flex flex-row justify-between">
+            <p className="text-xl font-semibold text-black">
+              {car?.spending}L/day
+            </p>
+            <p className="text-xl font-semibold text-black">Manual</p>
+          </div>
+        </div>
+        <div className="flex justify-between items-center w-full">
+          <p className="lg:text-2xl text-xl font-semibold text-black">
+            ${car?.price}/day
+          </p>
+          <Button
+            className="lg:h-[56px] lg:w-[140px] h-[44px] w-[100px] bg-blue-600"
+            variant="default"
+            size="default"
+          >
+            Rent Now
+          </Button>
+        </div>
       </div>
     </div>
   );
