665201033fe640d5d449dcdc82a8ad205dd8cf73
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-12-18 22:11:46 +0200

    feat: add payment method and confirmation forms, integrate them into the payment page, and update car data with saloon images.

--- src/app/payment/[id]/page.tsx
+++ src/app/payment/[id]/page.tsx
@@ -1,9 +1,12 @@
-import { NavigationBar } from "@/components";
-import { AboutUs } from "@/components";
-import { Cars } from "@/lib/Cars-data";
-import { RentalSummary } from "@/components";
-import { BillingInfo } from "@/components";
-import { RentalInfo } from "@/components";
+import {
+  NavigationBar,
+  PayMethod,
+  AboutUs,
+  RentalSummary,
+  BillingInfo,
+  RentalInfo,
+  ConfirmationForm,
+} from "@/components";
 
 export default async function Home({ params }: { params: { id: string } }) {
   const id = (await params).id;
@@ -11,10 +14,12 @@ export default async function Home({ params }: { params: { id: string } }) {
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto] items-start justify-center font-[family-name:var(--font-geist-sans)">
       <NavigationBar />
-      <div className="flex lg:flex-row flex-col justify-between lg:p-8 p-4 w-full">
+      <div className="flex lg:flex-row flex-col-reverse justify-between lg:p-8 p-4 w-full">
         <div className="flex flex-col gap-4">
           <BillingInfo />
           <RentalInfo />
+          <PayMethod />
+          <ConfirmationForm />
         </div>
         <RentalSummary id={id} name={""} image={""} price={""} />
       </div>
